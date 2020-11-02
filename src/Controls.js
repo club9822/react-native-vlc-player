ae48dd29fec95702f384e3e6edc3f256e17dcada
حامد سهیلی <48531597+club9822@users.noreply.github.com>
2020-11-02 16:18:00 +0330

    Update Controls.js

--- src/Controls.js
+++ src/Controls.js
@@ -1,47 +1,55 @@
 import React, { Component } from 'react'
-import { Platform, StyleSheet, View, TouchableNativeFeedback, TouchableWithoutFeedback } from 'react-native'
+import { Platform, StyleSheet, View, TouchableNativeFeedback, TouchableWithoutFeedback,Dimensions } from 'react-native'
 import PropTypes from 'prop-types'
 import Icon from 'react-native-vector-icons/Ionicons'
 
 const isAndroid = Platform.OS === 'android'
-
+const screenWidth=Dimensions.get('window').width
 export default class Controls extends Component {
   render() {
     const { overlay, height, width, controls } = this.props
 
-    return (
-      <TouchableWithoutFeedback onPress={this._handleOverlay}>
-        <View
-          opacity={overlay.opacity}
-          style={[ styles.overlay, {
-            backgroundColor: overlay.backgroundColor,
-            height         : height,
-            width          : width
-          } ]}>
-
-          <TouchableNativeFeedback
-            background={isAndroid ? TouchableNativeFeedback.SelectableBackground() : ''}
-            onPress={this._handlePlay}>
-            <View
-              style={styles.containerPlay}>
-              <Icon
-                color='white'
-                name={controls.pause ? 'md-play' : 'md-pause'}
-                size={30} />
-            </View>
-          </TouchableNativeFeedback>
-
-          <TouchableNativeFeedback
-            background={isAndroid ? TouchableNativeFeedback.SelectableBackground() : ''}
-            onPress={this._handleOrientation}>
-            <View
-              style={styles.containerToggle}>
-              <Icon color='white' name={controls.toggle} size={30} />
-            </View>
-          </TouchableNativeFeedback>
-        </View>
-      </TouchableWithoutFeedback>
-    )
+    return (<TouchableWithoutFeedback
+        onPress={this._handlePlay}>
+      <View
+          style={styles.containerPlay}>
+        <Icon
+            color='white'
+            name={controls.pause ? 'md-play' : 'md-pause'}
+            size={screenWidth*0.1} />
+      </View>
+    </TouchableWithoutFeedback>)
+    // return (
+    //   <TouchableWithoutFeedback onPress={this._handleOverlay}>
+    //     <View
+    //       opacity={overlay.opacity}
+    //       style={[ styles.overlay, {
+    //         backgroundColor: overlay.backgroundColor,
+    //         height         : height,
+    //         width          : width
+    //       } ]}>
+    //
+    //       <TouchableWithoutFeedback
+    //         onPress={this._handlePlay}>
+    //         <View
+    //           style={styles.containerPlay}>
+    //           <Icon
+    //             color='white'
+    //             name={controls.pause ? 'md-play' : 'md-pause'}
+    //             size={30} />
+    //         </View>
+    //       </TouchableWithoutFeedback>
+    //
+    //       {/*<TouchableWithoutFeedback*/}
+    //       {/*  onPress={this._handleOrientation}>*/}
+    //       {/*  <View*/}
+    //       {/*    style={styles.containerToggle}>*/}
+    //       {/*    <Icon color='white' name={controls.toggle} size={30} />*/}
+    //       {/*  </View>*/}
+    //       {/*</TouchableWithoutFeedback>*/}
+    //     </View>
+    //   </TouchableWithoutFeedback>
+    // )
   }
 
   _handleOverlay = () => {
